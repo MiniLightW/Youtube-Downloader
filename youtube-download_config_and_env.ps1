cf5b2ad0106e3189e505b8ac60abfedc91726375
MiniLightW <aubriet.damien@gmail.com>
2025-08-13 14:18:00 +0200

    optimize PS scripts : all prerequisites in one config file

---
+++ youtube-download_config_and_env.ps1
@@ -0,0 +1,59 @@
+# Youtube-Downloader - Configuration Script
+
+
+Write-Host "+---------------------------------------------------------------------+"
+Write-Host "Loading config..."
+
+# custom section : set path
+$OutPath = "$env:HOMEDRIVE$env:HOMEPATH\Downloads\Youtube-Downloader"
+# end custom section
+
+$ProjectPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$ScriptPath = Join-Path $ProjectPath "youtube-download.py"
+$VenvPath = Join-Path $ProjectPath "venv"
+$activate = Join-Path $VenvPath "Scripts\Activate.ps1"
+
+Write-Host "Loading config : OK"
+
+
+# Youtube-Downloader - Check Environment
+Write-Host "+---------------------------------------------------------------------+"
+Write-Host "Check Environment prerequisites..."
+
+# check if python is installed
+if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
+    Write-Host "Python is not installed. Please install Python to use this script."
+    Pause
+    Exit
+}
+# check if script path exists
+if (-not (Test-Path $ScriptPath)) {
+    Write-Host "The script path does not exist. Please check the path."
+    Pause
+    Exit
+}
+# Check if output path exists, if not create it
+if (-not (Test-Path $OutPath)) {
+    New-Item -ItemType Directory -Path $OutPath | Out-Null
+    if (-not (Test-Path $OutPath)) {
+        Write-Host "Failed to create output directory. Please check your permissions."
+        Pause
+        Exit
+    }
+}
+Write-Host "Check Environment prerequisites : OK"
+
+# Activate the virtual environment
+Write-Host "+---------------------------------------------------------------------+"
+Write-Host "Activating virtual environment..."
+& $activate
+
+$pythonPath = Get-Command python | Select-Object -ExpandProperty Source
+if ($pythonPath -like "*venv*python.exe") {
+    Write-Host "Activating virtual environment : OK"
+} else {
+    Write-Host "Executable Python used : $pythonPath"
+    Write-Host "Activating virtual environment : FAIL"
+    Pause
+    exit
+}
--- youtube-download_video_shorts.ps1
+++ youtube-download_video_shorts.ps1
@@ -4,43 +4,15 @@
 # script section 
 ######################################################################################
 
-# load config
-. "$PSScriptRoot\youtube-download_config.ps1"
-
-# start script
-Write-Host "+---------------------------------------------------------------------+"
-# check if python is installed
-if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
-    Write-Host "Python is not installed. Please install Python to use this script."
-    Pause
-    Exit
-}
-# check if script path exists
-if (-not (Test-Path $ScriptPath)) {
-    Write-Host "The script path does not exist. Please check the path."
-    Pause
-    Exit
-}
-# Check if output path exists, if not create it
-if (-not (Test-Path $OutPath)) {
-    New-Item -ItemType Directory -Path $OutPath | Out-Null
-    if (-not (Test-Path $OutPath)) {
-        Write-Host "Failed to create output directory. Please check your permissions."
-        Pause
-        Exit
-    }
-}
-
-# Activate the virtual environment
-Write-Host "Activating virtual environment..."
-$activate
+# load config and env
+. "$PSScriptRoot\youtube-download_config_and_env.ps1"
 
 # show configuration
+Write-Host "+---------------------------------------------------------------------+"
 Write-Host "Configuration :"
 Write-Host "Output folder is here : $OutPath"
 Write-Host "Script path is here   : $ScriptPath"
 Write-Host "!!! THIS SCRIPT IS FOR YOUTUBE SHORTS DOWNLOAD ONLY !!!"
-
 Write-Host "+---------------------------------------------------------------------+"
 $url = Read-Host "Write the Youtube URL or playlist"
 if ($url -eq "") {
